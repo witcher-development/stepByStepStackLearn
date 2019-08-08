45ce981dfdfa1ceb756d01a23ce3185ae6bb8b64
Witcher <roman.web.kuznietsov@gmail.com>
2019-08-08 09:59:51 +0300

    Styled components, PropTypes and start Menu component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-class App extends React.Component {
+import Menu from './components/Menu';
+
+const AppWrapper = styled.div`
+  position:relative;
+  
+  padding-top: 50px;
+`;
+
+const StyledMenu = styled(Menu)`
+  width: 100%;
+  height: 50px;
+  
+  position:fixed;
+  top: 0;
+  left: 0;
+`;
+
+class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.var = 'start';
+    this.theme = 'light';
+    this.taskList = [];
   }
 
   render() {
+    const { theme, taskList } = this;
+
     return (
-      <div className="App">
-        { this.var }
-      </div>
+      <AppWrapper>
+        <StyledMenu theme={theme}/>
+      </AppWrapper>
     );
   }
 }
---
+++ client/src/components/Menu.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Menu extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const { className, theme } = this.props;
+
+		return (
+			<div className={className}>
+				{ theme }
+			</div>
+		);
+	}
+}
+
+Menu.propTypes = {
+	theme: PropTypes.string.isRequired,
+};
+
+export default Menu;
+
