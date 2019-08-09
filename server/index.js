3e2865b71dc4c3c81018bf574d58d78d755800f2
Witcher <roman.web.kuznietsov@gmail.com>
2019-08-09 09:42:41 +0300

    trying to connect firestore

--- server/index.js
+++ server/index.js
@@ -1,2 +1,40 @@
 const express = require('express');
-const app = express();
+// const firebase = require("firebase/app");
+
+const admin = require('firebase-admin');
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBOdT_RT23sDEcoUkSXRDgEUcVWTxrPRag",
+	authDomain: "stack-learn.firebaseapp.com",
+	databaseURL: "https://stack-learn.firebaseio.com",
+	projectId: "stack-learn",
+	storageBucket: "",
+	messagingSenderId: "728880840563",
+	appId: "1:728880840563:web:1ca1536b628ad6ae"
+};
+// firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+	firebaseConfig
+});
+
+const db = admin.firestore();
+db.collection('tasks').get()
+	.then(res => {
+		console.log(res);
+	})
+	.catch(e => console.log(e));
+
+// const app = express();
+//
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyBOdT_RT23sDEcoUkSXRDgEUcVWTxrPRag",
+// 	authDomain: "stack-learn.firebaseapp.com",
+// 	databaseURL: "https://stack-learn.firebaseio.com",
+// 	projectId: "stack-learn",
+// 	storageBucket: "",
+// 	messagingSenderId: "728880840563",
+// 	appId: "1:728880840563:web:1ca1536b628ad6ae"
+// };
+// firebase.initializeApp(firebaseConfig);
+//
+// const database = firebase.database();
