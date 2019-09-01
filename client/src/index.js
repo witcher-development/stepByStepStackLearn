5c2fd47dafc2380d5924141419113776e15131d6
Witcher <roman.web.kuznietsov@gmail.com>
2019-09-01 21:49:00 +0300

    start new step. Init Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+	taskList: [],
+};
+
+const reducer = (initialState, action) => {
+
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
